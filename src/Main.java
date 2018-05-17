581f09c18f7b71d6841850b94484ef07c16330be
kj3002 <kevin.ji3002@gmail.com>
2018-05-17 19:34:30 -0400

    Radian-to-angle converter works + SinCosTan + LawOfSines + Law of Cosines

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) { //TODO: to be converted into something where the user just runs main to get desired program
+        System.out.println("Hello, World!");
+    }
+}
